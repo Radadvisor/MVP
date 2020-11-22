c633ff6a8c9c849db03f1b260dcc6c246fe1b486
u36703 <74767923+u36703@users.noreply.github.com>
2020-11-22 07:38:33 +0100

    Update settings.py

--- Radadvisor/settings.py
+++ Radadvisor/settings.py
@@ -122,5 +122,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 …
 # Configure Django App for Heroku.
-import django_heroku
-django_heroku.settings(locals())
+#import django_heroku
+#django_heroku.settings(locals())
